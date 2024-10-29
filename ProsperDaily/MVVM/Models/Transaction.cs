e86bc9cb44c087ddf5494a752b7208a19b2a6327
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-10-29 11:35:10 +0100

    Creating the model (By installing "sqlite-net-pcl" and "SQLiteNetExtensions" and "SQLitePCLRaw.provider.dynamic_cdecl" Nuget packages)

---
+++ ProsperDaily/MVVM/Models/Transaction.cs
@@ -0,0 +1,11 @@
+using ProsperDaily.Abstractions;
+
+namespace ProsperDaily.MVVM.Models;
+
+public class Transaction : TableData
+{
+    public string Name { get; set; }
+    public decimal Amount { get; set; }
+    public bool IsIncome { get; set; }
+    public DateTime OperationDate { get; set; }
+}
